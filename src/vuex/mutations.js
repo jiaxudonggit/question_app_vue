c40d1d03fb6fd4c083aa1fa91057b3c28b0d9e8d
JXD <Qazwsx23>
2021-04-19 04:43:08 +0800

    init

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -1,4 +1,4 @@
-import {Utils} from "@/utils/Utils";
+// noinspection JSUnresolvedVariable
 
 const mutations = {
 
@@ -31,59 +31,12 @@ const mutations = {
 
     },
 
-    // 设置应用配置
-    setAppStatus(state, data) {
-        // 基础配置
-        state.baseConfig = data.baseConfig;
-        state.baseConfig.centerAppId = String(state.baseConfig.centerAppId);
-        state.baseConfig.appId = String(state.baseConfig.appId);
-        state.baseConfig.appTypeId = String(state.baseConfig.appTypeId);
-        state.baseConfig.appThemeId = String(state.baseConfig.appThemeId);
-
-
-        // 主页配置
-        state.appConfig.indexConfig = data.indexConfig;
-        state.appConfig.indexConfig.detail = [
-            {data: data.indexConfig.subjectNumber, unit: '道', desc: "测试题目"},
-            {data: data.indexConfig.peopleNumber, unit: 'W', desc: "参与测试"},
-            {data: data.indexConfig.praiseRate, unit: '%', desc: "好评率"},
-        ];
-
-        // 答题配置
-        state.appConfig.answerConfig = data.answerConfig;
-
-        // 结果配置
-        state.appConfig.resultConfig = data.resultConfig;
-
-        // 弹幕配置
-        for (let i = 0; i < data.barrageConfig.barrageList.length; i++) {
-            data.barrageConfig.barrageList[i].avatar = Utils.makeBarrageAvatarUrl(data.barrageConfig.barrageList[i].avatar);
-        }
-        state.barrageConfig = data.barrageConfig;
-
-        // 推荐配置
-        state.recommendConfig = data.recommendConfig;
-    },
-
     // 设置用户信息
     setUserInfo(state, userInfo) {
         state.nickname = String(userInfo.nickname);
         state.headImage = String(userInfo.head_img_url);
         state.sex = userInfo.sex;
         state.accessToken = userInfo.access_token;
-    },
-
-
-    // 设置推荐配置
-    setRecommendConfig(state, recommendConfig) {
-        state.recommendConfig = recommendConfig;
-    },
-
-    // 更新推荐配置
-    updateRecommendConfig(state, recommendConfig) {
-        state.recommendConfig.total_page = recommendConfig.total_page;
-        state.recommendConfig.page = recommendConfig.page;
-        state.recommendConfig.recommendArray = state.recommendConfig.recommendArray.concat(recommendConfig.recommendArray);
     },
 
 }
