8f0269f60e81f9f8a1be66919080e196cf1738b1
jiaxudong <Qazwsx123>
2021-04-30 11:31:02 +0800

    删除无用的引用

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -8,7 +8,7 @@ Vue.use(Vuex)
 
 const state = {
     // 工程配置相关
-    debug: true,
+    debug: false,
     isRunBrowser: window.nativeObj === undefined,
     appApiUrl: "http://interest.ezhigame.com", // 后端接口地址（正式服）
     testAppApiUrl: "http://interest.test.ezhigame.com", // 后端接口地址(测试服)
@@ -34,7 +34,7 @@ const state = {
     debugUserId: "01234567890",
     // 阅友单独使用的数据
     signStr: "bi6tUL8Tf31I+DHx9KRJkVP1M7S0jqQ9vTnUjDARxI6APhslppch7qofwgz8ikdJ2xzY4t8dsH2mhto4s1bHk7+X196BWZ+Iut7dby1dqECTMK17xOAOOb8ABT1AmHuaSIMIrsWicJ43f4TNzC+WF4jTUksL9FiqLYstgS0auAY=",      // 验签串
-    centerAppId: "700086", // 700086=测试服  700085=正式服
+    centerAppId: "700085", // 700086=测试服  700085=正式服
     // UI相关
     isAppending: false, // 加载动画
     isGameBack: false,  // 推荐弹窗
